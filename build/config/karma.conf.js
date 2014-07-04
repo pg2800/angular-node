445b0dd1dd33a5d4f3272f858be8bc6c5ef8316a
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-07-04 14:58:18 -0700

    Adding configuration file for Karma

---
+++ build/config/karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function (config){
+	config.set({
+		basePath: '../'
+		,files: [ 
+		//init files
+			//required
+			'vendor/angular/v1.2.15/angular.min.js' 
+			,'vendor/angular/v1.2.15/angular-mocks.js'
+			//src
+			,'src/breadthFirst/module.js'
+			,'src/breadthFirst/routes.js'
+			,'src/breadthFirst/algorithm.js'
+			//specs
+			,'specs/breadthFirst/module.spec.js'
+			,'specs/breadthFirst/routes.spec.js'
+			,'specs/breadthFirst/algorithm.spec.js'
+		//end files
+		]
+		,autoWatch: false
+		,frameworks: ['jasmine']
+		,browsers: ['Chrome']
+	});
+};
