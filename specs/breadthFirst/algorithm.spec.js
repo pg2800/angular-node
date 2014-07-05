dc14692fe750df5c7ccaf8d28abf60ea6a45e5ca
Pablo Ivan Garcia Camou <pg_2800@hotmail.com>
2014-07-04 17:10:15 -0700

    Tests spec

---
+++ specs/breadthFirst/algorithm.spec.js
@@ -0,0 +1,9 @@
+describe("Breadth First Algorithm", function() {
+	beforeEach(module('breadthFirst'));
+	describe("Functionality", function() {
+		it("should behave...", inject(function (){
+			expect(undefined).toBeUndefined();
+		}));
+	});
+	
+});
